c43f60388cb8c3c3eedfd35723428cec175edb79
OneGoodReas0n <korobovi4@gmail.com>
2019-10-12 23:23:35 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function multiply(first, second) {
-  // your solution
+function multiply(first, second) {
+  let array = []
+
+  const firstArr = first.split('').reverse()
+  const secondArr = second.split('').reverse()
+
+  for (let i = 0; i < firstArr.length; i++) {
+    for (let j = 0; j < secondArr.length; j++) {
+      multiply = firstArr[i] * secondArr[j]
+      array[i + j] = array[i + j] ? array[i + j] + multiply : multiply
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    const unit = array[i] % 10
+    const decimal = Math.floor(array[i] / 10)
+    array[i] = unit
+    if (array[i + 1]) {
+      array[i + 1] += decimal
+    }
+    else if (decimal !== 0) {
+      array[i + 1] = decimal
+    }
+  }
+  return array.reverse().join('')
 }
+
+module.exports = multiply
